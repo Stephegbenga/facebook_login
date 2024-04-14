a45c6ad3dbae0a7c45aa8b96594b7a8a67eae7dd
Stephegbenga <adegbamiyestephen2018@gmail.com>
2024-04-14 14:14:47 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,44 @@
+import logo from './logo.svg';
+import './App.css';
+import FacebookLogin from 'react-facebook-login';
+
+function App() {
+
+  const responseFacebook = (response) => {
+    console.log(response); // Handle the response from Facebook login
+    // You can send the response to your backend for further processing
+  };
+
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+
+
+      <div>
+      <h2>Login with Facebook</h2>
+      <FacebookLogin
+        appId="7550976808327371"
+        autoLoad={false}
+        fields="name,email,picture"
+        callback={responseFacebook}
+      />
+    </div>
+    </div>
+  );
+}
+
+export default App;
