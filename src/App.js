1ecb229c091e180388e685d516800bafddf3186b
Stephegbenga <adegbamiyestephen2018@gmail.com>
2024-04-15 09:24:42 +0100

    used a youtube code

--- src/App.js
+++ src/App.js
@@ -1,42 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
-import FacebookLogin from 'react-facebook-login';
+import { useState } from "react";
+import { LoginSocialFacebook } from "reactjs-social-login";
+import { FacebookLoginButton } from "react-social-login-buttons";
 
 function App() {
-
-  const responseFacebook = (response) => {
-    console.log(response); // Handle the response from Facebook login
-    // You can send the response to your backend for further processing
-  };
-
+  const [profile, setProfile] = useState(null);
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div>
+      {!profile ? (
+        <LoginSocialFacebook
+          appId="1089471788931508"
+          onResolve={(response) => {
+            console.log(response);
+            setProfile(response.data);
+          }}
+          onReject={(error) => {
+            console.log(error);
+          }}
         >
-          Learn React
-        </a>
-      </header>
+          <FacebookLoginButton />
+        </LoginSocialFacebook>
+      ) : (
+        ""
+      )}
 
-
-      <div>
-      <h2>Login with Facebook</h2>
-      <FacebookLogin
-        appId="1089471788931508"
-        autoLoad={false}
-        fields="name,email,picture"
-        callback={responseFacebook}
-      />
-    </div>
+      {profile ? (
+        <div>
+          <h1>{profile.name}</h1>
+          <img src={profile.picture.data.url} />
+        </div>
+      ) : (
+        ""
+      )}
     </div>
   );
 }
